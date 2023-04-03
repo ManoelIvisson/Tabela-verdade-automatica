eadf6f8acbdf0ed3d33e560973d3a4baa4d40626
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-04-03 10:59:46 -0300

    Construindo a tabela

--- script/cria_tabela.js
+++ script/cria_tabela.js
@@ -1,2 +1,20 @@
-tabela = window.document.getElementById("ctabela")
-tabela.innerHTML += '<tr><td>oloco</td</tr>'
+function cria_tabela(){
+    let i = 0;
+
+    linha = window.document.getElementById("ctabela")
+    quant_proposicoes = window.document.getElementById("iproposicao")
+    for (; i < (2 ** quant_proposicoes.value);i++){
+        linha.innerHTML += '<tr id="ln' + i +'"></tr>'
+        linha_atual = window.document.getElementById("ln" + i)
+        for (let i = 0; i < quant_proposicoes.value; i++) {
+            linha_atual.innerHTML += '<td>Uepaaaaa</td>'
+        }
+    }
+}
+
+function colunas(num) {
+    for (let i = 0; i < num; i++) {
+        alert(i)
+        return '<td>Uepaaaaa</td>'
+    }
+}
