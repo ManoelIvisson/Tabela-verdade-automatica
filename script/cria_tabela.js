f10c21c23dfe5cf70523803671e8bc447a868bdd
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-04-10 08:24:48 -0300

    atualizando a tabela

--- script/cria_tabela.js
+++ script/cria_tabela.js
@@ -1,13 +1,17 @@
 function cria_tabela(){
     let i = 0;
 
+    //Deixa a tabela vazia
+    window.document.getElementById('ctabela').innerHTML = '' 
+
     linha = window.document.getElementById("ctabela")
+
     quant_proposicoes = window.document.getElementById("iproposicao")
     for (; i < (2 ** quant_proposicoes.value);i++){
         linha.innerHTML += '<tr id="ln' + i +'"></tr>'
         linha_atual = window.document.getElementById("ln" + i)
         for (let i = 0; i < quant_proposicoes.value; i++) {
-            linha_atual.innerHTML += '<td>Uepaaaaa</td>'
+            linha_atual.innerHTML += '<td>V</td>'
         }
     }
 }
